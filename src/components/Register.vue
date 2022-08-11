dcbc23606908b278ac3efb4d831970ea533f7377
Sandra Bayabos <48815451+SandraBayabos@users.noreply.github.com>
2022-08-11 22:42:32 +0800

    ft/register: installed axios and logic for registration

---
+++ src/components/Register.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="green-bg"></div>
+  <div class="form-container">
+    <div id="login-form" class="shadow-sm">
+      <form @submit.prevent="handleSubmit">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            v-model="username"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            placeholder="Email"
+            v-model="email"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="password"
+          />
+        </div>
+
+        <button>Sign Up</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "RegisterForm",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const data = {
+        name: this.username,
+        email: this.email,
+        password: this.password,
+      };
+      console.log(data);
+
+      axios
+        .post("http://localhost:3001/auth/signup", data)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+#green-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 40vw;
+  background-color: rgb(81, 90, 80);
+  height: 100%;
+}
+
+.form-container {
+  height: calc(100vh - 60px);
+  position: relative;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#login-form {
+  width: 500px;
+  height: 300px;
+  background-color: rgb(237, 238, 223);
+  padding: 30px;
+  border-radius: 10px;
+  /* box-shadow: ; */
+}
+
+form {
+  width: 100%;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginForm from "./components/Login.vue";
+import RegisterForm from "./components/Register.vue";
 
-const routeInfos = [{ path: "/login", component: LoginForm }];
+const routeInfos = [
+  { path: "/login", component: LoginForm },
+  { path: "/register", component: RegisterForm },
+];
 
 const router = createRouter({
   history: createWebHistory(),
